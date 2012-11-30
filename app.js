122b225a280c35805751662545917ca8edbe4663
Thomas A. Valletta <tom@vallettas.com>
2012-11-29 22:26:38 -0700

    Night before RWX

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(process.env.VCAP_APP_PORT || 3000);
+
+//app.configure(function(){
+//    app.use(express.bodyParser());
+//});
+
+app.use("/", express.static(__dirname + '/static'));
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('connection', {});
+
+    socket.on('message', function(msg){
+        console.log('message', msg);
+        socket.broadcast.emit('msg', msg);
+    });
+
+    socket.on('remote value', function(data) {
+		socket.broadcast.emit('remote', data);
+    });
+});
